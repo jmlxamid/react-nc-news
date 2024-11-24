d3571f4f7e6b95faf6a972a8a7aa8d0bf42b60ef
ABDI BASHIR <AX@Hakims-MacBook-Air.local>
2024-11-24 21:57:24 +0000

    loading animation included

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import Lottie from "lottie-light-react";
+import loadingHand from "../src/assets/animations/loading-animation.json";
 import "../src/index.css";
 
-const Loading = () => <p>Loading...</p>;
+const Loading = () => {
+  return (
+    <section className="center-layout">
+      <article className="lottie-container">
+        <div className="lottie">
+          <Lottie animationData={loadingHand} loop={true} />
+        </div>
+      </article>
+      <p>Loading, please wait...</p>
+    </section>
+  );
+};
 
 export default Loading;
